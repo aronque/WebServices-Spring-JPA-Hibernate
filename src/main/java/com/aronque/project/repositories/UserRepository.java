46637accdf243dcd69a0b3d1aa3472ccb3715234
Vitor Siwerski Aronque <vitorsiwerskiaronque@gmail.com>
2020-11-28 17:45:32 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/aronque/project/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.aronque.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.aronque.project.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+}
