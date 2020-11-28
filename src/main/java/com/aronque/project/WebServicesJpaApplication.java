4ac02ccdec144511b4d8f1944feec61b5143c5ec
Vitor Siwerski Aronque <vitorsiwerskiaronque@gmail.com>
2020-11-28 17:11:50 -0300

    Project Created

---
+++ src/main/java/com/aronque/project/WebServicesJpaApplication.java
@@ -0,0 +1,13 @@
+package com.aronque.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aronque/project/WebServicesJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aronque.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
