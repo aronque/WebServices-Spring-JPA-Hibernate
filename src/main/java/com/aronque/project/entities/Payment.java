8ccbe80a594d6d88c273a02b421d8d165b320481
Vitor Siwerski Aronque <vitorsiwerskiaronque@gmail.com>
2021-01-18 15:46:17 -0300

    subTotal and Total methods

--- src/main/java/com/aronque/project/entities/Payment.java
+++ src/main/java/com/aronque/project/entities/Payment.java
@@ -11,6 +11,8 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name = "tb_payment")
 public class Payment implements Serializable{
@@ -21,6 +23,7 @@ public class Payment implements Serializable{
 	private Long id;
 	private Instant moment;
 	
+	@JsonIgnore
 	@OneToOne
 	@MapsId
 	private Order order;
@@ -49,7 +52,7 @@ public class Payment implements Serializable{
 	public void setMoment(Instant moment) {
 		this.moment = moment;
 	}
-
+	 
 	public Order getOrder() {
 		return order;
 	}
